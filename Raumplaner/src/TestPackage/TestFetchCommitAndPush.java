b734ae214f0cc26170e1007f3b89d4189213219a
Feridun Mekec <fm039@hdm-stuttgart.de>
2014-10-22 13:24:22 +0200

    class to check the functionalty from Commit, Push etc.

---
+++ Raumplaner/src/TestPackage/TestFetchCommitAndPush.java
@@ -0,0 +1,11 @@
+package TestPackage;
+
+public class TestFetchCommitAndPush {
+	// Servus Jungs!
+	// Die Klasse dient dazu, ob bei jedem die o.g. Optionen funktionieren.
+	// bitte testen, ob ihr mit Fetch die aktualisierten Dateien bekommt und versuchen hier einen Commit sowie nen Push zu
+	// machen.
+	// @Fabian bitte nicht die Klasse löschen, bevor wir uns getroffen haben und sicher sind, dass bei jedem alles klappt.
+	// GRüüüüße, Feridun.
+
+}
